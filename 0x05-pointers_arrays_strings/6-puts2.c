9b0e71d5e71fcdf91534bd41a6265916c54d8ce8
KoomsonJessica32 <koomsonjessica32@gmail.com>
2023-02-27 15:05:41 -0800

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,18 +5,18 @@
  * @str: input
  * Return: print
  */
-
 void puts2(char *str)
 {
 	int i = 0;
 
-	for (i : str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
-			_putchar([i]);
+			_putchar(str[i]);
 		else
-
 			continue;
 	}
 	_putchar('\n');
 }
+
+

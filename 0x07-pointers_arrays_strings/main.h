fddb1dba8e1b9a2a19bd10889c66203fb3c8cd8f
KoomsonJessica32 <koomsonjessica32@gmail.com>
2023-03-04 19:37:24 -0800

    Task Zero

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#include "main.h"
+#ifndef MAIN_H
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
@@ -10,5 +10,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif
